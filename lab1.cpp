c19ec9df98fe81e771331402bdc51f249e1f095b
Grell6068 <6068011grell@gmail.com>
2024-09-18 19:40:09 +0300

    lab1.cpp

---
+++ lab1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+int number;
+cout << "Enter number";
+cin >> number;
+
+if(number>=1 && number<=100){cout<<"True";}
+else{cout<<"False";}
+}
